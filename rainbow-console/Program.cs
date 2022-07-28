c6abc5dcf19e32429e059d6a5027b1b150a7e2e6
dudushy <eduardotalarico1337@gmail.com>
2022-07-28 17:30:19 -0300

    First working version

---
+++ rainbow-console/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace RainbowConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insert a text: ");
+            string text = Console.ReadLine();
+            Console.Write("\n");
+            while (true)
+            {
+                foreach (char c in text)
+                {
+                    Console.ForegroundColor = (ConsoleColor)Enum.GetValues(typeof(ConsoleColor)).GetValue((new Random()).Next(1, 15)); // except black(0)
+                    Console.BackgroundColor = (ConsoleColor)Enum.GetValues(typeof(ConsoleColor)).GetValue((new Random()).Next(1, 15)); // all colors
+                    Console.Write(c);
+                }
+            }
+        }
+    }
+}
